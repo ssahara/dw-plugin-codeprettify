bbd349a12ce5a0ac4f6af2d3295410392c2d2d2b
Eric Knibbe <eric@lassosoft.com>
2015-06-29 11:01:28 -0400

    revise <code> regex

--- syntax/code.php
+++ syntax/code.php
@@ -43,7 +43,7 @@ class syntax_plugin_codeprettify_code extends DokuWiki_Syntax_Plugin {
                 $match = substr($match, 5, -1);
                 list($params, $title) = explode('|', $match);
                 $class['prettify'] = 'prettyprint';
-                if (preg_match('/(?:^[: ](?!linenums)|(?: lang[-:]))(\w+)/', $params, $m)) {
+                if (preg_match('/(?:^:| (?:lang[-:])?)(?!linenums)(\w+)/', $params, $m)) {
                     $class['language'] = 'lang-'.$m[1];
                 }
                 if (preg_match('/ linenums(:\d+)?/', $params, $m)) {
