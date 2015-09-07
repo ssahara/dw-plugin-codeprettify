53847f7db93dc016fc9bf133b0b36fbf453691b0
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-09-07 21:41:45 +0900

    typo fix

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -11,7 +11,7 @@
 $lang['override'] = 'override &lt;code&gt; syntax to show the block pretty-printed';
 
 $lang['url_prettify_handlers'] = 'Base URL for prettify.js and additional language handler scripts<br />(ex: https://cdn.rawgit.com/google/code-prettify/master/src/)';
-$lanf['url_prettify_skins']    = 'Base URL for color themes for code-prettify (css)<br />(ex: https://cdn.rawgit.com/google/code-prettify/master/styles/)';
+$lang['url_prettify_skins']    = 'Base URL for color themes for code-prettify (css)<br />(ex: https://cdn.rawgit.com/google/code-prettify/master/styles/)';
 
 $lang['lang_handlers']  = 'additional langage handler (comma separated)';
 $lang['skin']           = 'color theme/skin name for prettify (CSS filename in code-prettify/styles/)';
