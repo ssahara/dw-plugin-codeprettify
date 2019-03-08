77ddd0cbf9bdfcf9420c3b35a913fc517f3fecaa
Satoshi Sahara <sahara.satoshi@gmail.com>
2019-03-08 21:34:30 +0900

    code-prettify 2019-03-06

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -11,8 +11,8 @@
 $lang['override'] = 'override &lt;code&gt; syntax to show the block pretty-printed';
 $lang['linenums'] = 'enable line numbering implicitly';
 
-$lang['url_prettify_handlers'] = 'Base URL for prettify.js and additional language handler scripts<br /><small>(ex: https://cdn.rawgit.com/google/code-prettify/master/src/)</small>';
-$lang['url_prettify_skins']    = 'Base URL for color themes for code-prettify (css)<br /><small>(ex: https://cdn.rawgit.com/google/code-prettify/master/styles/)</small>';
+$lang['url_prettify_handlers'] = 'Base URL for prettify.js and additional language handler scripts<br /><small>(ex: https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/)</small>';
+$lang['url_prettify_skins']    = 'Base URL for color themes for code-prettify (css)<br /><small>(ex: https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/skins/)</small>';
 
 $lang['lang_handlers']  = 'additional langage handlers (comma separated)';
 $lang['skin']           = 'color theme for prettify (CSS filename in code-prettify/styles/)';
